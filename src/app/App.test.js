bb78d735fa2aa609f258573cf9ab19b538b3dd1c
dreamerTram <mbigombaraktram@gmail.com>
2018-08-30 02:54:22 +0300

    chore: configure enzyme for testing  - install enzyme and related depencies  - add tests setup file  - update tests;

--- src/app/App.test.js
+++ src/app/App.test.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import App from "./App";
+
+describe("App", () => {
+  const wrapper = shallow(<App />);
+
+  test("renders App component", () => {
+    expect(wrapper.exists()).toBeTruthy();
+  });
 });
