b63d781519b0497fcbf4b1e2a1565838e3c04e07
dreamerTram <mbigombaraktram@gmail.com>
2018-09-01 14:27:16 +0300

    feature: add loader component

--- src/dashboard/dashboard-container.jsx
+++ src/dashboard/dashboard-container.jsx
@@ -4,7 +4,9 @@ import Dashboard from "./dashboard";
 import { loadCategories } from "./actions";
 
 const mapStateToProps = state => ({
-  categories: state.categories.data
+  categories: state.categories.data,
+  isLoading: state.categories.isLoading,
+  hasError: state.categories.hasError.status
 });
 
 const mapDispatchTpProps = dispatch => ({
--- src/dashboard/dashboard.jsx
+++ src/dashboard/dashboard.jsx
@@ -1,25 +1,34 @@
 import React from "react";
 import styled from "styled-components";
-import { func, arrayOf, string } from "prop-types";
+import { func, arrayOf, string, bool } from "prop-types";
 
 import { PrimaryButton } from "../components/buttons/buttons";
 import CategoryList from "./category-list/category-list";
+import Loader from "../components/Loader/Loader";
 
 const DashboardContainer = styled.div`
   display: flex;
   flex-direction: column;
 `;
 
-const Dashboard = ({ categories, loadCategories }) => (
+const Dashboard = ({ categories, isLoading, hasError, loadCategories }) => (
   <DashboardContainer>
     <PrimaryButton onClick={loadCategories}>Search For Jokes</PrimaryButton>
-    <CategoryList categories={categories} />
+    {isLoading ? (
+      <Loader />
+    ) : hasError ? (
+      "Error Loading categories"
+    ) : (
+      <CategoryList categories={categories} />
+    )}
   </DashboardContainer>
 );
 
 Dashboard.propTypes = {
   loadCategories: func,
-  categories: arrayOf(string)
+  categories: arrayOf(string),
+  isLoading: bool,
+  hasError: bool
 };
 
 export default Dashboard;
