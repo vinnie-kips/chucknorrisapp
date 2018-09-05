08d17d4fe39a2bc33d91e8f35967f68401b04c2d
dreamerTram <mbigombaraktram@gmail.com>
2018-09-05 19:05:00 +0300

    fix: update redux devtools error

--- src/root/store.js
+++ src/root/store.js
@@ -10,7 +10,9 @@ const store = createStore(
   reducer,
   compose(
     applyMiddleware(sagaMiddleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : f => f
   )
 );
 
